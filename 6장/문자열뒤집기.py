ba7cd35eda916dad4693aa5e0a89e9d957cc63fb
minho <59383800+pot1223@users.noreply.github.com>
2022-07-30 17:09:19 +0900

    Update 문자열뒤집기.py

--- 6장/문자열뒤집기.py
+++ 6장/문자열뒤집기.py
@@ -2,8 +2,17 @@
 
 def reverse_str(s:str):
   reverse=[]
-  reverse = str[::-1]
+  reverse = s[::-1] #공간복잡도가 0(n)이 된다 
   return reverse
+
+# 슬라이싱을 이용한 풀이2
+
+def reverse_str(s:str):
+  reverse=[]
+  s[:] = s[::-1] # 이 방식을 사용하면 공간복잡도가 0(1)이 된다  
+  reverse s 
+  return reverse
+
 
 # 위치 포인터를 이용한 풀이
 
@@ -18,7 +27,7 @@ def reverse_pt(s:str):
   
  #파이썬 메소드를 이용한 풀이 
  
- def reverse_md(s:str):
+def reverse_md(s:str):
   s.reverse()
   return s 
   
