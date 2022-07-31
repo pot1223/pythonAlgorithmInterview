0b3e7de0dae0d4418d4279e144e30b0889f7a835
minho <59383800+pot1223@users.noreply.github.com>
2022-07-31 10:20:14 +0900

    Create 로그파일 재정렬.py

---
+++ 6장/로그파일 재정렬.py
@@ -0,0 +1,17 @@
+import re 
+
+def resort(s:list):
+  resortlst1 = []
+  resortlst2 = []
+  resortlst3 = []
+  for i in range(len(s)):
+    if list(s[i][-1]) == re.findall('[a-zA-Z]',s[i][-1]):
+      resortlst1.append(s[i])
+      resortlst1.sort(key=lambda x :x.split()[1])
+    else:
+      resortlst2.append(s[i]) 
+  finalresortlst = resortlst1 + resortlst2
+  return finalresortlst
+  
+logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+resort(logs)
