11e4120cfb739d8f80021ece76f05f1d170c00da
minho <59383800+pot1223@users.noreply.github.com>
2022-08-02 15:58:50 +0900

    Update 그룹 애너그램.py

--- 6장/그룹 애너그램.py
+++ 6장/그룹 애너그램.py
@@ -1,1 +1,17 @@
+from collections import OrderedDict
 
+lst = ["eat", "tea", "tan" ,"ate" ,"nat","bat"]
+
+def anagram(s:list):
+  analst = []
+  flist=[]
+  result = []
+  for i in range(len(s)):
+    analst.append("".join(sorted(s[i])))
+  for i in range(len(analst)):
+    flist.append([ s[j] for j in range(len(analst)) if analst[i]==analst[j]])
+  for value in flist:
+    if value not in result:
+        result.append(value)
+  return result
+anagram(lst)
