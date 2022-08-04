0fc48905185614559053b6be4fa8febec71cd609
so_minho <59383800+pot1223@users.noreply.github.com>
2022-08-04 09:38:28 +0900

    Update 두 수의 합.py

--- 7장/두 수의 합.py
+++ 7장/두 수의 합.py
@@ -1,3 +1,5 @@
+# 브루트 포스법으로 해결한 문제로 시간복잡도가 0(n2)나 된다 
+
 def returnsumindex(lst : list, target : int ):
   flist = []
   for i in range(len(lst)):
@@ -12,3 +14,34 @@ def returnsumindex(lst : list, target : int ):
 nums =[2,7,11,15]
 target =100
 returnsumindex(nums,target)
+
+# in을 이용한 풀이 
+def twoSum(nums:list, target:int):
+   for i, n in enumerate(nums): 
+      complement = tanget - n 
+      if complement in nums[i+1:]: #in 연산을 이용한 풀이는 0(n2) 시간복잡도라도 브루트 포스법보다 훨씬 더 가볍고 빠르게 풀 수 있다 
+        return nums.index(n), nums[i+1:].index(complement)+(i+1)
+     
+# dictionary를 이용한 풀이 
+
+def twoSum(nums:list, target: int):
+  nums_map={}
+  for i, num in enumerate(nums):
+    nums_map[num] = i # num값을 키로 하고 i를 인덱스로 하는 딕셔너리를 만든다 
+  for i, num in enumerate(nums):
+    if target-num in nums_map and i != nums_map[target -num]: #전체적인 시간복잡도는 0(n)이 된다 
+      return nums.index(num), nums_map[target-num] # 딕셔너리로 조회하므로 평균적으로 시간복잡도가 0(1)이 된다 
+     
+# dictionary를 이용한 풀이2
+
+def twoSum(nums:list, target: int):
+  nums_map ={}
+  for i, num in enumerate(nums):
+    if target -num in nums_map:
+      return [nums_map[target-num],i]
+    nums_map[num] = i 
+      
+      
+      
+      
+      
