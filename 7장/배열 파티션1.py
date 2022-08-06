41a2bfa8bc3f30c131798b418400cdf6247afa46
so_minho <59383800+pot1223@users.noreply.github.com>
2022-08-07 08:27:36 +0900

    Update 배열 파티션1.py

--- 7장/배열 파티션1.py
+++ 7장/배열 파티션1.py
@@ -1,3 +1,5 @@
+# 내림차순 정렬 후 2개 요소마다 min값 더하기  
+
 lst = [2,5,7,4,3,2]
 def arraypartition(s:lst):
   s.sort(reverse= True)
@@ -10,4 +12,26 @@ def arraypartition(s:lst):
     i+=2
   return k  
     
+arraypartition(lst)
+
+# 내림차순 정렬 후 짝수 번째 합 계산 
+
+lst = [2,5,7,4,3,2]
+def arraypartition(s:lst):
+  s.sort(reverse= True)
+  sum = 0
+  for i,n in enumerate(s):
+    if i % 2 ==0:
+      sum += n
+  return sum 
+
+arraypartition(lst)
+
+
+# 오름차순 정렬 후 파이썬다운 방법 사용 
+
+lst = [2,5,7,4,3,2]
+def arraypartition(s:lst):
+  return sum(sorted(s)[::2])
+
 arraypartition(lst)
