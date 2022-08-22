ae41a5e046f9e2a91b82a589fab59010cbc0a7ce
so_minho <59383800+pot1223@users.noreply.github.com>
2022-08-22 16:42:53 +0900

    Update 원형 데크 디자인.py

--- 10장/원형 데크 디자인.py
+++ 10장/원형 데크 디자인.py
@@ -6,44 +6,44 @@ class LinkedList:
 class MyCircularDeque:
 
   def __init__(self, k:int):
-    self.front = LinkedList(None)
+    self.front = LinkedList(None) # front 노드 앞에 rear 노드가 위치하게 만든다. 데크는 front 노드와 rear노드 사이에 들어간다 
     self.rear = LinkedList(None)
     self.k, self.len = k, 0  
     self.front.right, self.rear.left = self.rear, self.front
   
-  def _add(self, node:LinkedList, new:LinkedList):
+  def _add(self, node:LinkedList, new:LinkedList): # 노드 앞에 새로운 노드를 넣는다 
     exist = node.right
     node.right = new 
     new.left, new.right =node, exist 
     exist.left = new
   
-  def _del(self, node : LinkedList):
+  def _del(self, node : LinkedList): # 해당 노드 앞에 있는 노드를 삭제한다 
     exist = node.right.right
     node.right = exist
     exist.left = node 
   
-  def insertFront(self, value : int):
+  def insertFront(self, value : int): # front노드 앞에 새로운 노드를 삽입한다 
     if self.len == self.k:
       return False
     self.len += 1
     self._add(self.front, LinkedList(value))
     return True 
   
-  def insertLast(self, value : int):
+  def insertLast(self, value : int): # rear노드 뒤에 새로운 노드를 삽입한다 
     if self.len == self.k:
       return False
     self.len +=1 
     self._add(self.rear.left, LinkedList(value))
     return True
 
-  def deleteFront(self):
+  def deleteFront(self): # front노드 앞에 있는 노드를 삭제한다 
     if self.len == 0:
       return False
     self.len -=1 
     self._del(self.front)
     return True 
 
-  def deleteLast(self):
+  def deleteLast(self): # rear 노드 뒤에 있는 노드를 삭제한다 
     if self.len == 0:
       return False
     self.len -= 1
