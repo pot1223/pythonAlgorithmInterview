8b7ce37e480b26ac0fbf979bfd01dc7a1e132235
so_minho <59383800+pot1223@users.noreply.github.com>
2022-08-26 14:59:21 +0900

    Create 중복 문자 없는 가장 긴 부분 문자열.py

---
+++ 11장/중복 문자 없는 가장 긴 부분 문자열.py
@@ -0,0 +1,15 @@
+s = "abcabcbb"
+
+def lengthOflonger(s:str):
+  used= {}
+  max_length = start = 0 
+  for index, char in enumerate(s):
+    if char in used and start <= used[char]:
+      start = used[char] +1 
+    else:
+      max_length = max(max_length , index-start+1)
+    used[char] = index
+    print(used)
+  return max_length 
+
+lengthOflonger(s)
