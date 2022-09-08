84792a6d47fb075f338c3131c0683c99e46acac5
so_minho <59383800+pot1223@users.noreply.github.com>
2022-09-08 23:28:36 +0900

    Update 이진 트리의 최대 깊이.py

--- 14장/이진 트리의 최대 깊이.py
+++ 14장/이진 트리의 최대 깊이.py
@@ -1,22 +1,22 @@
 import collections
 
-class TreeNode :
+class TreeNode : 
   def __init__(self , val = 0 , left = None , right = None) :
     self.val = val 
     self.left = left 
     self.right = right 
 
 def maxDepth(root : TreeNode) : 
-  if root is None : 
+  if root is None : # 노드가 아무것도 없으면 0을 반환 
     return 0
-  queue = collections.deque([root])
+  queue = collections.deque([root]) # 트리노드 객체를 리스트로 변환한 후 데크 객체 생성 
   depth = 0 
   while queue : 
     depth += 1 
-    for _ in range(len(queue)) : 
-      cur_root = queue.popleft() 
-      if cur_root.left :
-        queue.append(cur_root.left)
+    for _ in range(len(queue)) : # 루트 노드 개수만큼 
+      cur_root = queue.popleft() # 루트 노드를 pop 
+      if cur_root.left : # pop된 노드의 왼쪽 자식 노드가 있으면 
+        queue.append(cur_root.left) # 데크에 추가 
       if cur_root.right : 
         queue.append(cur_root.right)
   return depth
