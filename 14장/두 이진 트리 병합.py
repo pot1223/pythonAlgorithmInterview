e09ede44ba611a116429b6851319d81dbb7abfbc
so_minho <59383800+pot1223@users.noreply.github.com>
2022-09-13 22:11:38 +0900

    Update 두 이진 트리 병합.py

--- 14장/두 이진 트리 병합.py
+++ 14장/두 이진 트리 병합.py
@@ -7,8 +7,8 @@ class TreeNode :
 def mergeTree(t1 : TreeNode , t2 : TreeNode) : 
   if t1 and t2 : 
     node = TreeNode(t1.val + t2.val)
-    node.left = mergeTree(t1.left , t2.left)
-    node.right = mergeTree(t1.right , t2.right)
+    node.left = mergeTree(t1.left , t2.left) # 왼쪽으로 끝까지 더하기 
+    node.right = mergeTree(t1.right , t2.right) # 오른쪽으로 끝까지 더하기 
     return node
   else :
     return t1 or t2
